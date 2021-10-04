a8373401fa8ccd4499bd8ad61e53a56d3391a64f
saeed <saeed>
2021-10-04 15:13:15 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png">  
+  <table>
+    <thead>
+        <th>شماره</th>
+        <th> نام شخص</th>
+        <th>تاریخ</th>
+        <th>عنوان</th>
+        <th>فیلد</th>
+        <th>قبلی</th>
+        <th>جدید</th>
+    </thead>    
+    <tbody>      
+        <row  v-for="item in dataCache" :key="item.id" :changelog="item"/>             
+    </tbody>
+</table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import row from './components/row.vue'
+import inputData from './assets/data.json'
+const step = 20;
+var lastCachedItem = step;
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    row
+  }, methods:{    
+    loadMore(){
+      if(window.scrollMaxY == window.scrollY){
+        console.log('calling handleScroll');
+        lastCachedItem += step;
+        this.dataCache = inputData.slice(0,lastCachedItem)
+      }
+    }
+  },
+  data:function () {
+    let cache = inputData.slice(0,step)
+    return{
+      changelogs : inputData,
+      dataCache: cache
+    }
+  },
+  created(){
+    console.log('mounted')
+    window.addEventListener('scroll', this.loadMore);
+  },     
+  unmounted () {
+    window.removeEventListener('scroll', this.loadMore);
+  },
 }
 </script>
 
 <style>
+
+@font-face {
+  font-family: "vazir";
+  src: local("vazir"),   url("./assets/fonts/vazir.ttf") format("truetype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  direction: rtl;  
+  font-family: "vazir";
+}
+table{
+  border: 1px solid gray;
+}
+thead{
+  background-color: #ffe26a;
+}
+tr{
+  border-top: 1px solid lightgray;
 }
 </style>
---
+++ src/components/row.vue
@@ -0,0 +1,28 @@
+<template>  
+  <tr>
+    <td>{{changelog.id}}</td>    
+    <td>{{changelog.name}}</td>    
+    <td>{{changelog.date}}</td>    
+    <td>{{changelog.title}}</td>    
+    <td>{{changelog.field}}</td>    
+    <td>{{changelog.old_value}}</td>    
+    <td>{{changelog.new_value}}</td>    
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'row',
+  props: {
+    changelog: Object
+  }
+}
+</script>
+
+<style>
+
+td {  
+  text-align: center;
+  border-left: 1px solid lightgray;
+}
+</style>
